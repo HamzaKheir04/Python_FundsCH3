4cece9819557cad84f29005abc4a0d54de5c6e21
Hamza Kheir <hamzaalhabashnah@gmail.com>
2025-07-06 20:36:52 +0300

    Add files via upload

---
+++ BONUSES/Bonuses.py
@@ -0,0 +1,10 @@
+"""
+The Question :
+
+print this string as is by escaping characters as needed :
+
+this is an f-string in python, it looks like this: "f"{var_name}
+ 
+"""
+
+print(f"This is an F_String in Pythin it Looks Like this : f\'{{Variable_Name}}\'")
